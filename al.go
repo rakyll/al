fb8feed23dbe4bcf3c89ff583a14f154f0b89424
Burcu Dogan <jbd@google.com>
2014-12-18 18:58:00 -0800

    Initial commit.

---
+++ al.go
@@ -0,0 +1,80 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package al
+
+/*
+#cgo darwin  LDFLAGS: -framework OpenAL
+
+#include <stdlib.h>
+#ifdef GOOS_darwin
+#include <OpenAL/al.h>
+#endif
+*/
+
+import "C"
+
+type Buffer int32
+
+func GenBuffer() Buffer {
+	panic("not yet implemented")
+}
+
+func (b Buffer) Valid() bool {
+	panic("not yet implemented")
+}
+
+func (b Buffer) Channels() int32 {
+	panic("not yet implemented")
+}
+
+func (b Buffer) Size() int32 {
+	panic("not yet implemented")
+}
+
+func (b Buffer) Bits() int32 {
+	panic("not yet implemented")
+}
+
+func (b Buffer) AppendData(format int32, freq int32, data []byte) error {
+	panic("not yet implemented")
+}
+
+type Source int32
+
+func GenSource() Source {
+	panic("not yet implemented")
+}
+
+func PlaySources(s []Source) error {
+	panic("not yet implemented")
+}
+
+func StopSources(s []Source) error {
+	panic("not yet implemented")
+}
+
+func PauseSources(s []Source) error {
+	panic("not yet implemented")
+}
+
+func RewindSources(s []Source) error {
+	panic("not yet implemented")
+}
+
+func DopplerFactor() float32 {
+	panic("not yet implemented")
+}
+
+func DopplerVelocity() float32 {
+	panic("not yet implemented")
+}
+
+func SpeedOfSound() float32 {
+	panic("not yet implemented")
+}
+
+func DistanceModel() int32 {
+	panic("not yet implemented")
+}
---
+++ const.go
@@ -0,0 +1,15 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package al
+
+const (
+	DISTANCE_MODEL            = 0xD000
+	INVERSE_DISTANCE          = 0xD001
+	INVERSE_DISTANCE_CLAMPED  = 0xD002
+	LINEAR_DISTANCE           = 0xD003
+	LINEAR_DISTANCE_CLAMPED   = 0xD004
+	EXPONENT_DISTANCE         = 0xD005
+	EXPONENT_DISTANCE_CLAMPED = 0xD006
+)
