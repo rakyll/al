6db7ace7e475e43f7e56c93be8e0782f2afa17c4
Burcu Dogan <jbd@google.com>
2015-01-05 12:06:46 -0800

    Reorder CFLAGS.

--- al.go
+++ al.go
@@ -7,7 +7,9 @@ package al
 // docker run -v $GOPATH/src:/src mobile /bin/bash -c 'cd /src/github.com/rakyll/al && CGO_ENABLED=1 GOARCH=arm GOARM=7  GOOS=android  go build  .'
 
 /*
+#cgo darwin   CFLAGS: -DGOOS_darwin
 #cgo android  CFLAGS: -DGOOS_android -DANDROID
+
 #cgo android  CFLAGS: -DMAX_SOURCES_LOW=4 -DMAX_SOURCES_START=8 -DMAX_SOURCES_HIGH=64
 #cgo android  CFLAGS: -marm -DOPENAL_FIXED_POINT -DOPENAL_FIXED_POINT_SHIFT=16 -DPOST_FROYO -DAL_ALEXT_PROTOTYPES -fpic -ffunction-sections -DANDROID -funwind-tables -fstack-protector -DHAVE_GCC_VISIBILITY -O3 -fno-short-enums -g
 #cgo android  LDFLAGS: -Wl,--build-id -Bsymbolic -lm -llog -ldl
@@ -17,7 +19,6 @@ package al
 #endif
 
 #cgo darwin  LDFLAGS: -framework OpenAL
-#cgo darwin  CFLAGS: -DGOOS_darwin
 
 #ifdef GOOS_darwin
 #include <OpenAL/al.h>
