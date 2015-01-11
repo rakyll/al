f0933ea4ad61ed8f99c557c5eee81dddf2d9b123
Burcu Dogan <jbd@google.com>
2015-01-11 12:55:00 -0800

    Reorg repo.

--- al.go
+++ al.go
@@ -25,6 +25,7 @@ package al
 #endif
 */
 import "C"
+import "unsafe"
 
 func Enable(capability int32) {
 	C.alEnable(C.ALenum(capability))
@@ -73,4 +74,164 @@ func Error() uint32 {
 
 func toBool(v C.ALboolean) bool {
 	return int32(v) == kTrue
+}
+
+type Source int32
+
+func GenSources(n int) []Source {
+	s := make([]C.ALuint, n)
+	C.alGenSources(C.ALsizei(n), (*C.ALuint)(unsafe.Pointer(&s[0])))
+	r := make([]Source, n)
+	for i, v := range s {
+		r[i] = Source(v)
+	}
+	return r
+}
+
+func PlaySources(source ...Source) {
+	panic("not yet implemented")
+}
+
+func PauseSources(source ...Source) {
+	panic("not yet implemented")
+}
+
+func StopSources(source ...Source) {
+	panic("not yet implemented")
+}
+
+func RewindSources(source ...Source) {
+	panic("not yet implemented")
+}
+
+func DeleteSources(source ...Source) {
+	panic("not yet implemented")
+}
+
+func (s Source) Gain() float32 {
+	panic("not yet implemented")
+}
+
+func (s Source) SetGain(v float32) {
+	panic("not yet implemented")
+}
+
+func (s Source) MaxGain() float32 {
+	panic("not yet implemented")
+}
+
+func (s Source) SetMaxGain(v float32) {
+	panic("not yet implemented")
+}
+
+func (s Source) MinGain() float32 {
+	panic("not yet implemented")
+}
+
+func (s Source) SetMinGain(v float32) {
+	panic("not yet implemented")
+}
+
+func (s Source) Position() Vector {
+	panic("not yet implemented")
+}
+
+func (s Source) SetPosition(v Vector) {
+	panic("not yet implemented")
+}
+
+func (s Source) QueueBuffers(buffer ...Buffer) {
+	panic("not yet implemented")
+}
+
+// TODO(jbd): Add SetPosition.
+//
+/*
+ * LISTENER
+ * Listener represents the location and orientation of the
+ * 'user' in 3D-space.
+ *
+ * Properties include: -
+ *
+ * Gain         AL_GAIN         ALfloat
+ * Position     AL_POSITION     ALfloat[3]
+ * Velocity     AL_VELOCITY     ALfloat[3]
+ * Orientation  AL_ORIENTATION  ALfloat[6] (Forward then Up vectors)
+ */
+
+type Vector struct {
+	X, Y, Z float32
+}
+
+type Orientation struct {
+	Forward Vector
+	Up      Vector
+}
+
+type Listener struct{}
+
+func (l Listener) Gain() float32 {
+	panic("not yet implemented")
+}
+
+func (l Listener) Position() Vector {
+	panic("not yet implemented")
+}
+
+func (l Listener) Velocity() Vector {
+	panic("not yet implemented")
+}
+
+func (l Listener) Orientation() Orientation {
+	panic("not yet implemented")
+}
+
+func (l Listener) SetGain(v float32) {
+	panic("not yet implemented")
+}
+
+func (l Listener) SetPosition(v Vector) {
+	panic("not yet implemented")
+}
+
+func (l Listener) SetVelocity(v Vector) {
+	panic("not yet implemented")
+}
+
+func (l Listener) SetOrientation(o Orientation) {
+	panic("not yet implemented")
+}
+
+type Buffer int32
+
+func GenBuffers(n int) []Buffer {
+	panic("not yet imeplemented")
+}
+
+func DeleteBuffers(buffer ...Buffer) {
+	panic("not yet imeplemented")
+}
+
+func (b Buffer) Freq() int32 {
+	panic("not yet implemented")
+}
+
+func (b Buffer) Channels() int32 {
+	panic("not yet implemented")
+}
+
+func (b Buffer) Size() int32 {
+	panic("not yet implemented")
+}
+
+func (b Buffer) Bits() int32 {
+	panic("not yet implemented")
+}
+
+func (b Buffer) SetData(format int32, data []byte, freq int32) int32 {
+	panic("not yet implemented")
+}
+
+func (b Buffer) Valid() bool {
+	panic("not yet implemented")
 }
